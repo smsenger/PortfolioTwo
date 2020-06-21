5474b48cca99bd2a328a58a91c6ea4934043fdac
Saesha Senger <saesha.senger@gmail.com>
2020-06-21 15:39:43 -0400

    adds changes to addToWatchlist function that includes button. in progress

--- sceneIt/scene-it-starter/watchlist.js
+++ sceneIt/scene-it-starter/watchlist.js
@@ -2,15 +2,11 @@
 const movieContainer = document.getElementsByClassName("movies-container")[0];
 const movie = document.getElementsByClassName("movie")[0];
 
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-// });
-        function renderMovies(movieArray) {
-            //take in array of movies
-            //return string of HTML like that in step 1
-            let movieHtmlArray = movieArray.map(function(currentMovie){
-                return `<div class="card m-3" style="width:12rem">
+function renderMovies(movieArray) {
+    //take in array of movies
+    //return string of HTML like that in step 1
+    let movieHtmlArray = movieArray.map(function (currentMovie) {
+        return `<div class="card m-3" style="width:12rem">
                 <img class="card-img-top" id="poster" src="${currentMovie.Poster}"/>
                             <div class="card-body">
                             <div class="card-title d-flex justify-content-between">
@@ -20,19 +16,17 @@ const movie = document.getElementsByClassName("movie")[0];
                             <div id="b"><button onclick="removeFromWatchList('${currentMovie.imdbID}')" class="btn btn-primary btn-sm">Remove Movie</button></div>
                             </div>
                             </div>`
-                        })
-                        return movieHtmlArray.join(``);
-                    }
-                    movie.innerHTML = renderMovies(JSON.parse(localStorage.getItem('watchlist')));
-
-
+    })
+    return movieHtmlArray.join(``);
+}
+movie.innerHTML = renderMovies(JSON.parse(localStorage.getItem('watchlist')));
 
 
 function removeFromWatchList(imdbID) {
     let watchlistJSON = localStorage.getItem('watchlist');
     let watchlist = JSON.parse(watchlistJSON);
     console.log(watchlist);
-    watchlist = watchlist.filter(function(movie) {    
+    watchlist = watchlist.filter(function (movie) {
         return movie.imdbID != imdbID
     });
     console.log(watchlist);
